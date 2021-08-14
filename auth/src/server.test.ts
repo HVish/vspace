ff39ae7ae9678d0766fcc52102491c97cfcebca1
Vishnu Singh <vishnusingh1729@gmail.com>
2021-08-15 02:09:18 +0530

    update test

--- auth/src/server.test.ts
+++ auth/src/server.test.ts
@@ -4,10 +4,8 @@ import server from './server';
 
 const request = supertest(server);
 
-describe('Test server', () => {
-  it('should respond to /ping with a pong message', async () => {
-    const response = await request.get('/ping');
-    expect(response.status).toBe(200);
-    expect(response.text).toBe('pong');
-  });
+test('ping-pong test', async () => {
+  const response = await request.get('/ping');
+  expect(response.status).toBe(200);
+  expect(response.text).toBe('pong');
 });
