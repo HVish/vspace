e5a17b917d2fe4957372783b81c984d9b7401e84
Vishnu Singh <vishnusingh1729@gmail.com>
2021-09-05 02:38:07 +0530

    add validation middleware to core package

---
+++ core/src/middlewares.test.ts
@@ -0,0 +1,106 @@
+import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import Joi from 'joi';
+import { validate } from './middlewares';
+
+describe('validate middleware', () => {
+  const mockResponse = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn().mockReturnThis(),
+  };
+
+  beforeEach(() => {
+    mockResponse.status.mockClear();
+    mockResponse.json.mockClear();
+  });
+
+  it('should return a function', () => {
+    const middleware = validate({});
+    expect(typeof middleware).toBe('function');
+  });
+
+  it('should send 412 status code', () => {
+    const middleware = validate({
+      query: Joi.object({
+        test: Joi.string().required(),
+      }),
+    });
+
+    const mockRequest = { query: {} };
+
+    middleware(
+      mockRequest as Request,
+      mockResponse as unknown as Response,
+      jest.fn()
+    );
+
+    expect(mockResponse.status.mock.calls[0][0]).toBe(
+      StatusCodes.PRECONDITION_FAILED
+    );
+  });
+
+  it('should not call next()', () => {
+    const middleware = validate({
+      query: Joi.object({
+        test: Joi.string().required(),
+      }),
+    });
+
+    const mockRequest = { query: {} };
+    const next = jest.fn();
+
+    middleware(
+      mockRequest as Request,
+      mockResponse as unknown as Response,
+      next
+    );
+
+    expect(next.mock.calls.length).toBe(0);
+  });
+
+  it('response json should have property errors.query', () => {
+    const middleware = validate({
+      query: Joi.object({
+        test: Joi.string().required(),
+      }),
+    });
+
+    const mockRequest = { query: {} };
+
+    middleware(
+      mockRequest as Request,
+      mockResponse as unknown as Response,
+      jest.fn()
+    );
+
+    const param1 = mockResponse.json.mock.calls[0][0];
+
+    expect(param1).toHaveProperty('errors.query');
+    expect(param1.errors.query).toBeDefined();
+
+    expect(param1.errors.body).toBeUndefined();
+  });
+
+  it('response json should have property errors.body', () => {
+    const middleware = validate({
+      body: Joi.object({
+        test: Joi.string().required(),
+      }),
+    });
+
+    const mockRequest = { body: {} };
+
+    middleware(
+      mockRequest as Request,
+      mockResponse as unknown as Response,
+      jest.fn()
+    );
+
+    const param1 = mockResponse.json.mock.calls[0][0];
+
+    expect(param1).toHaveProperty('errors.body');
+    expect(param1.errors.body).toBeDefined();
+
+    expect(param1.errors.query).toBeUndefined();
+  });
+});
---
+++ core/src/middlewares.ts
@@ -0,0 +1,46 @@
+import { RequestHandler } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import { Schema } from 'joi';
+
+interface RequestValidationSchemaMap {
+  query?: Schema;
+  body?: Schema;
+}
+
+export const validate = (
+  schema: RequestValidationSchemaMap
+): RequestHandler => {
+  return (req, res, next) => {
+    const result = {
+      query: schema.query?.validate(req.query, { abortEarly: false }),
+      body: schema.body?.validate(req.body, { abortEarly: false }),
+    };
+
+    let hasErrors = false;
+
+    if (result.query?.error) {
+      hasErrors = true;
+      if (process.env.NODE_ENV === 'development') {
+        console.log(result.query.error.annotate());
+      }
+    }
+
+    if (result.body?.error) {
+      hasErrors = true;
+      if (process.env.NODE_ENV === 'development') {
+        console.log(result.body.error.annotate());
+      }
+    }
+
+    if (hasErrors) {
+      return res.status(StatusCodes.PRECONDITION_FAILED).json({
+        errors: {
+          query: result.query?.error?.toString(),
+          body: result.body?.error?.toString(),
+        },
+      });
+    }
+
+    next();
+  };
+};
