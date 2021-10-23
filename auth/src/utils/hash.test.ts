6556ebde5be07240279fabe3232c13ee3de16ae6
Vishnu Singh <vishnusingh1729@gmail.com>
2021-10-23 17:29:22 +0530

    optimise hash tests

--- auth/src/utils/hash.test.ts
+++ auth/src/utils/hash.test.ts
@@ -1,22 +1,24 @@
 import { Hash } from './hash';
 
 describe('Text Hashing', () => {
+  let hash1: string;
+  let hash2: string;
   const plainText = 'this is a plain text';
 
+  beforeAll(async () => {
+    hash1 = await Hash.create(plainText);
+    hash2 = await Hash.create(plainText);
+  });
+
   test('plain text should not match with hashed text', async () => {
-    const hash = await Hash.create(plainText);
-    expect(hash).not.toEqual(plainText);
+    expect(hash1).not.toEqual(plainText);
   });
 
   test('two hashes generated from same plain-text should not match', async () => {
-    const hash1 = await Hash.create(plainText);
-    const hash2 = await Hash.create(plainText);
     expect(hash1).not.toEqual(hash2);
   });
 
   test('compareToHash() should resolve to true', async () => {
-    const hash1 = await Hash.create(plainText);
-    const hash2 = await Hash.create(plainText);
     expect(Hash.compare(plainText, hash1)).resolves.toBe(true);
     expect(Hash.compare(plainText, hash2)).resolves.toBe(true);
   });
